b1bdb60280869cb77955deb7d7f08f61aed024c4
bienk <bianchi.andrea@iisgalvanimi.edu.it>
2020-04-09 17:33:27 +0000

    fine es 01.1

--- angularRouting/esercizio/src/app/spotify.service.ts
+++ angularRouting/esercizio/src/app/spotify.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 //Dichiaro che il servizio è iniettabile agli altri componenti a partire dal componente root
 @Injectable({
   providedIn: 'root'
 })
+
 export class SpotifyService {
    //url per oauth: https://developer.spotify.com/console/get-search-item/
   //Ottengo il modulo HttpClient
-  constructor(private http: HttpClient) { }
+
+
+  constructor(private http: HttpClient) {
+        const headers = new HttpHeaders({Authorization: environment.oauthToken});
+   }
 
   searchTrack(query: string) {
     const url = `https://api.spotify.com/v1/search?q=${query}&type=track`;
@@ -19,6 +25,16 @@ export class SpotifyService {
 
     let obsTracks = this.http.get(url, { headers });
     return obsTracks;
+    
  //Ritorno un observable ai componenti che richiedono il servizio
+  }
+   getTrack(id: string) {
+    const url = `https://api.spotify.com/v1/tracks/${id}`;
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer TUO_AUTH'
+    });
+    
+    return this.http.get(url, { headers });
   }
 }
